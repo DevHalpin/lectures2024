79587b93859558dc5d21fa1affb203282f98b6ca
David Halpin <devhalpin86@gmail.com>
2024-07-25 17:51:32 -0700

    June 10 Security and Real World Servers

---
+++ flex/10_06_All/mod3/w07/security_and_real_world_servers/server.js
@@ -0,0 +1,158 @@
+const express = require('express');
+const app = express()
+const PORT = 8000;
+const bcrypt = require('bcryptjs');
+const cookieSession = require('cookie-session');
+
+
+const langauge = require('./languages.json')
+
+// Middleware
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true})) // this lets req.body to populate
+app.use(cookieSession({
+  name: 'session',
+  keys: ['thisistotallynotasecret', 'neitheristhis']
+}));
+
+// Users to be login
+// Users DB
+const users = {
+  abc: {
+    id: "abc",
+    email: "a@a.com",
+    password: "123"
+  }, 
+  def: {
+    id: "def",
+    email: "b@b.com",
+    password: bcrypt.hashSync("123")
+  }
+}
+
+// Generate Random Id
+function randomString(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
+app.get('/', (req, res) => {
+  const lang = req.session.lang;
+  const templateVars = {
+    heading: langauge.homeHeadings[lang] || langauge.homeHeadings.en,
+    body: langauge.homeBodies[lang] || langauge.homeBodies.en
+  }
+  res.render('home', templateVars)
+})
+
+app.get('/about', (req, res) => {
+  const lang = req.session.lang;
+  if(!lang) {
+    return res.redirect('/')
+  }
+  const templateVars = {
+    head: langauge.aboutHeadings[lang],
+    body: langauge.aboutBodies[lang]
+  }
+  res.render('about', templateVars)
+})
+
+
+// Or means "This comes first " || "Use this instead"
+// || 
+// &&
+
+app.get('/langauges/:userLang', (req, res) => {
+  const lang = req.params.userLang
+  req.session.lang = lang
+  res.redirect('/')
+})
+
+app.post('/logout', (req, res) => {
+  // Remove/clear the cookie with this ID
+  req.session = null
+  res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  const {email, password} = req.body;
+
+  // Check to see if the user matches something in the DB
+  let foundUser = null;
+  for(let user in users) {
+    if(users[user].email === email && bcrypt.compareSync(password, users[user].password)) {
+      foundUser = users[user]
+    }
+  }
+
+  // incorrect 
+  if(!foundUser) {
+    return res.send("Your email or password does no match")
+  }
+
+  // Correct path
+  if(foundUser) {
+    // Set the cookie
+    req.session.userId = foundUser.id
+    return res.redirect('/')
+  }
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.put('/register', (req, res) => {
+  // const email = req.body.email;
+  // const password = req.body.password;
+  const {email, password} = req.body;
+  const hashedPassword = bcrypt.hashSync(password)
+
+  // Checking for email in DB
+  for(let user in users) {
+    if(users[user].email === email) {
+      res.send("Email is already in use")
+    }
+  }
+
+  const id = randomString(3, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+  const newUser = {
+    id,
+    email,
+    password: hashedPassword
+  }
+
+  users[id] = newUser;
+
+  console.log(users)
+
+  res.redirect('/')
+})
+
+app.get('/protected', (req, res) => {
+  // Only login users can see this page
+  // Check if there is a cookie
+  // req.body === get typed in data
+  // req.params === get URL data
+  // req.cookies == get cookies
+  console.log(req.session.userId);
+  const user = req.session.userId;
+  if(!user) {
+    return res.redirect('/')
+  }
+
+  const templateVars = {
+    user: users[user]
+  }
+  res.render('protected', templateVars)
+})
+
+app.listen(PORT, ( ) => {
+  console.log(`The app is listening on port ${PORT}`);
+})
